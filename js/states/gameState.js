6d3f0ae2be44fc76d53e8fe86e78f09c8dac3f74
Karim-308 <2001118@eng.asu.edu.eg>
2026-01-24 07:58:06 +0200

    Add restart button functionality and styles for level screens

--- js/states/gameState.js
+++ js/states/gameState.js
@@ -1,12 +1,16 @@
+import { setupLevel } from "../levels/level_screen.js";
+
 //menu button definition
 const menuBtn = document.querySelector(".menu-button");
 const statesScreen = document.querySelector(".states-screen");
+const levelScreen = document.querySelector(".level1-screen");
+const gameScreen = document.querySelector(".game-screen");
 
 //retry level button definition
 const retryBtn = document.querySelector(".retry-button");
 
-//next level button definition
-const nextBtn = document.querySelector(".next-button");
+//level screen restart
+const restartBtn = document.querySelector(".restart-button");
 
 //menu button logic
 menuBtn.addEventListener("click", () => {
@@ -20,17 +24,13 @@ menuBtn.addEventListener("click", () => {
 });
 
 //retry button logic
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
+retryBtn.addEventListener("click", () => {
+    statesScreen.style.display = "none";
+    levelScreen.style.display = "block";
+    setupLevel();
+});
 
-//next level button logic
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
-/////////////////////////
+//level screen restart
+restartBtn.addEventListener("click", () => {
+    setupLevel();
+});
